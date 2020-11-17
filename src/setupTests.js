3435e75af0c96c6dae3e9101c1acd2a4f2985937
Colin Fendrick <colin.fendrick@gmail.com>
2020-11-17 08:02:57 -0800

    Basic setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,21 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { pipe } from './helpers/generics';
+
+const withWrapper = (Wrapper, wrapperProps = {}) => children =>
+	<Wrapper {...wrapperProps}>{children}</Wrapper>;
+
+export const withRouter = withWrapper(BrowserRouter);
+
+export const renderWith = (...wrappers) => children => render(
+	pipe(...wrappers)(children)
+);
+
+export const setup = (...builtins) => (...fns) =>
+	builtins.forEach((builtin, ix) =>
+		builtin(() =>
+			fns[0].length ? fns[ix].forEach(f => f()) : fns.forEach(f => f())
+		)
+	);
