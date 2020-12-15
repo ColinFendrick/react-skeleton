8dc805f5d3c127787ce9e35e3757daa23083d469
Colin Fendrick <colin.fendrick@gmail.com>
2020-12-15 14:20:34 -0800

    Linting fix

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import { Home } from './components';
 
-const App = () => {
-	return (
-		<div>
+const App = () => (
+	<div>
       Basic Setup:
-			<Switch>
-				<Route path='/' exact component={Home} />
-			</Switch>
-		</div>
-
-	);
-};
+		<Switch>
+			<Route path='/' exact component={Home} />
+		</Switch>
+	</div>
+);
 
 export default App;
